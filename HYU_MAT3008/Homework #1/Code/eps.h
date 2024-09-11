0cf46b61b9b399801df683795c9f7aa71d5d6412
juwowa09 <juwowa09@gmail.com>
2024-09-11 22:51:23 +0900

    Numerical Analysis

---
+++ HYU_MAT3008/Homework #1/Code/eps.h
@@ -0,0 +1,7 @@
+void machar_double(int *ibeta, int *it, int *irnd, int *ngrd, int *machep, int *negep,
+                   int *iexp, int *minexp, int *maxexp, double *eps, double *epsneg,
+                   double *xmin, double *xmax);
+
+void machar(int *ibeta, int *it, int *irnd, int *ngrd, int *machep, int *negep,
+            int *iexp, int *minexp, int *maxexp, float *eps, float *epsneg,
+            float *xmin, float *xmax);
